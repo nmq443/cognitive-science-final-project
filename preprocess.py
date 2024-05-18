c59c0ccb0d6f54e614b7addc9f69feb517fa8b11
quang <21020564@vnu.edu.vn>
2024-05-18 14:36:48 +0700

    add some .py files

---
+++ preprocess.py
@@ -0,0 +1,178 @@
+import numpy as np
+import scipy.io as sio
+from sklearn.preprocessing import StandardScaler
+from sklearn.utils import shuffle
+from tensorflow.keras.utils import to_categorical
+
+def load_BCI2a_data(data_path, subject, training, all_trials = True):
+    """ Loading and Dividing of the data set based on the subject-specific 
+    (subject-dependent) approach.
+    In this approach, we used the same training and testing dataas the original
+    competition, i.e., 288 x 9 trials in session 1 for training, 
+    and 288 x 9 trials in session 2 for testing.  
+   
+        Parameters
+        ----------
+        data_path: string
+            dataset path
+            # Dataset BCI Competition IV-2a is available on 
+            # http://bnci-horizon-2020.eu/database/data-sets
+        subject: int
+            number of subject in [1, .. ,9]
+        training: bool
+            if True, load training data
+            if False, load testing data
+        all_trials: bool
+            if True, load all trials
+            if False, ignore trials with artifacts 
+    """
+    
+    # Define MI-trials parameters
+    n_channels = 22
+    n_tests = 6*48     
+    window_Length = 7*250 
+    
+    # Define MI trial window 
+    fs = 250          # sampling rate
+    t1 = int(1.5*fs)  # start time_point
+    t2 = int(6*fs)    # end time_point
+
+    class_return = np.zeros(n_tests)
+    data_return = np.zeros((n_tests, n_channels, window_Length))
+
+    NO_valid_trial = 0
+    if training:
+        a = sio.loadmat(data_path+'A0'+str(subject)+'T.mat')
+    else:
+        a = sio.loadmat(data_path+'A0'+str(subject)+'E.mat')
+    a_data = a['data']
+    for ii in range(0,a_data.size):
+        a_data1 = a_data[0,ii]
+        a_data2= [a_data1[0,0]]
+        a_data3= a_data2[0]
+        a_X         = a_data3[0]
+        a_trial     = a_data3[1]
+        a_y         = a_data3[2]
+        a_artifacts = a_data3[5]
+
+        for trial in range(0,a_trial.size):
+             if(a_artifacts[trial] != 0 and not all_trials):
+                 continue
+             data_return[NO_valid_trial,:,:] = np.transpose(a_X[int(a_trial[trial]):(int(a_trial[trial])+window_Length),:22])
+             class_return[NO_valid_trial] = int(a_y[trial])
+             NO_valid_trial +=1        
+    
+
+    data_return = data_return[0:NO_valid_trial, :, t1:t2]
+    class_return = class_return[0:NO_valid_trial]
+    class_return = (class_return-1).astype(int)
+
+    return data_return, class_return
+
+def standardize_data(X_train, X_test, channels): 
+    # X_train & X_test :[Trials, MI-tasks, Channels, Time points]
+    for j in range(channels):
+          scaler = StandardScaler()
+          scaler.fit(X_train[:, 0, j, :])
+          X_train[:, 0, j, :] = scaler.transform(X_train[:, 0, j, :])
+          X_test[:, 0, j, :] = scaler.transform(X_test[:, 0, j, :])
+
+    return X_train, X_test
+
+def load_data_LOSO (data_path, subject, dataset): 
+    """ Loading and Dividing of the data set based on the 
+    'Leave One Subject Out' (LOSO) evaluation approach. 
+    LOSO is used for  Subject-independent evaluation.
+    In LOSO, the model is trained and evaluated by several folds, equal to the 
+    number of subjects, and for each fold, one subject is used for evaluation
+    and the others for training. The LOSO evaluation technique ensures that 
+    separate subjects (not visible in the training data) are usedto evaluate 
+    the model.
+    
+        Parameters
+        ----------
+        data_path: string
+            dataset path
+            # Dataset BCI Competition IV-2a is available at 
+            # http://bnci-horizon-2020.eu/database/data-sets
+        subject: int
+            number of subject in [1, .. ,9/14]
+            Here, the subject data is used  test the model and other subjects data
+            for training
+    """
+    
+    X_train, y_train = [], []
+    for sub in range (0,9):
+        path = data_path+'s' + str(sub+1) + '/'
+        
+        if (dataset == 'BCI2a'):
+            X1, y1 = load_BCI2a_data(path, sub+1, True)
+            X2, y2 = load_BCI2a_data(path, sub+1, False)
+        # elif (dataset == 'CS2R'):
+        #     X1, y1, _, _, _  = load_CS2R_data_v2(path, sub, True)
+        #     X2, y2, _, _, _  = load_CS2R_data_v2(path, sub, False)
+        # elif (dataset == 'HGD'):
+        #     X1, y1 = load_HGD_data(path, sub+1, True)
+        #     X2, y2 = load_HGD_data(path, sub+1, False)
+        
+        X = np.concatenate((X1, X2), axis=0)
+        y = np.concatenate((y1, y2), axis=0)
+                   
+        if (sub == subject):
+            X_test = X
+            y_test = y
+        elif (X_train == []):
+            X_train = X
+            y_train = y
+        else:
+            X_train = np.concatenate((X_train, X), axis=0)
+            y_train = np.concatenate((y_train, y), axis=0)
+
+    return X_train, y_train, X_test, y_test
+
+def get_data(path, subject, dataset = 'BCI2a', classes_labels = 'all', LOSO = False, isStandard = True, isShuffle = True):
+    
+    # Load and split the dataset into training and testing 
+    if LOSO:
+        """ Loading and Dividing of the dataset based on the 
+        'Leave One Subject Out' (LOSO) evaluation approach. """ 
+        X_train, y_train, X_test, y_test = load_data_LOSO(path, subject, dataset)
+    else:
+        """ Loading and Dividing of the data set based on the subject-specific 
+        (subject-dependent) approach.
+        In this approach, we used the same training and testing data as the original
+        competition, i.e., for BCI Competition IV-2a, 288 x 9 trials in session 1 
+        for training, and 288 x 9 trials in session 2 for testing.  
+        """
+        if (dataset == 'BCI2a'):
+            path = path + 's{:}/'.format(subject+1)
+            X_train, y_train = load_BCI2a_data(path, subject+1, True)
+            X_test, y_test = load_BCI2a_data(path, subject+1, False)
+        # elif (dataset == 'CS2R'):
+        #     X_train, y_train, _, _, _ = load_CS2R_data_v2(path, subject, True, classes_labels)
+        #     X_test, y_test, _, _, _ = load_CS2R_data_v2(path, subject, False, classes_labels)
+        # elif (dataset == 'HGD'):
+        #     X_train, y_train = load_HGD_data(path, subject+1, True)
+        #     X_test, y_test = load_HGD_data(path, subject+1, False)
+        else:
+            raise Exception("'{}' dataset is not supported yet!".format(dataset))
+
+    # shuffle the data 
+    if isShuffle:
+        X_train, y_train = shuffle(X_train, y_train,random_state=42)
+        X_test, y_test = shuffle(X_test, y_test,random_state=42)
+
+    # Prepare training data     
+    N_tr, N_ch, T = X_train.shape 
+    X_train = X_train.reshape(N_tr, 1, N_ch, T)
+    y_train_onehot = to_categorical(y_train)
+    # Prepare testing data 
+    N_tr, N_ch, T = X_test.shape 
+    X_test = X_test.reshape(N_tr, 1, N_ch, T)
+    y_test_onehot = to_categorical(y_test)    
+    
+    # Standardize the data
+    if isStandard:
+        X_train, X_test = standardize_data(X_train, X_test, N_ch)
+
+    return X_train, y_train, y_train_onehot, X_test, y_test, y_test_onehot
---
+++ train.py
@@ -0,0 +1,157 @@
+import os
+import time
+import shutil
+import numpy as np
+import tensorflow as tf
+from sklearn.metrics import cohen_kappa_score
+from sklearn.model_selection import train_test_split
+from preprocess import get_data
+
+def train(dataset_conf, train_conf, results_path):
+    
+    # remove the 'result' folder before training
+    if os.path.exists(results_path):
+        # Remove the folder and its contents
+        shutil.rmtree(results_path)
+        os.makedirs(results_path)        
+
+    # Get the current 'IN' time to calculate the overall training time
+    in_exp = time.time()
+    # Create a file to store the path of the best model among several runs
+    best_models = open(results_path + "/best models.txt", "w")
+    # Create a file to store performance during training
+    log_write = open(results_path + "/log.txt", "w")
+    
+    # Get dataset paramters
+    dataset = dataset_conf.get('name')
+    n_sub = dataset_conf.get('n_sub')
+    data_path = dataset_conf.get('data_path')
+    isStandard = dataset_conf.get('isStandard')
+    LOSO = dataset_conf.get('LOSO')
+    # Get training hyperparamters
+    batch_size = train_conf.get('batch_size')
+    epochs = train_conf.get('epochs')
+    patience = train_conf.get('patience')
+    lr = train_conf.get('lr')
+    LearnCurves = train_conf.get('LearnCurves') # Plot Learning Curves?
+    n_train = train_conf.get('n_train')
+    model_name = train_conf.get('model')
+    from_logits = train_conf.get('from_logits') 
+
+    # Initialize variables
+    acc = np.zeros((n_sub, n_train))
+    kappa = np.zeros((n_sub, n_train))
+    
+    # Iteration over subjects 
+    # for sub in range(n_sub-1, n_sub): # (num_sub): for all subjects, (i-1,i): for the ith subject.
+    for sub in range(n_sub): # (num_sub): for all subjects, (i-1,i): for the ith subject.
+
+        print('\nTraining on subject ', sub+1)
+        log_write.write( '\nTraining on subject '+ str(sub+1) +'\n')
+        # Initiating variables to save the best subject accuracy among multiple runs.
+        BestSubjAcc = 0 
+        bestTrainingHistory = [] 
+        
+        # Get training and test data
+        X_train, _, y_train_onehot, _, _, _ = get_data(
+            data_path, sub, dataset, LOSO = LOSO, isStandard = isStandard)
+         
+        # Divide the training data into training and validation
+        X_train, X_val, y_train_onehot, y_val_onehot = train_test_split(X_train, y_train_onehot, test_size=0.2, random_state=42)       
+        
+        # Iteration over multiple runs 
+        for train in range(n_train): # How many repetitions of training for subject i.
+            # Set the random seed for TensorFlow and NumPy random number generator. 
+            # The purpose of setting a seed is to ensure reproducibility in random operations. 
+            tf.random.set_seed(train+1)
+            np.random.seed(train+1)
+            
+            # Get the current 'IN' time to calculate the 'run' training time
+            in_run = time.time()
+            
+            # Create folders and files to save trained models for all runs
+            filepath = results_path + '/saved models/run-{}'.format(train+1)
+            if not os.path.exists(filepath):
+                os.makedirs(filepath)        
+            filepath = filepath + '/subject-{}.h5'.format(sub+1)
+            
+            # Create the model
+            model = getModel(model_name, dataset_conf, from_logits)
+            # Compile and train the model
+            model.compile(loss=CategoricalCrossentropy(from_logits=from_logits), optimizer=Adam(learning_rate=lr), metrics=['accuracy'])          
+
+            # model.summary()
+            # plot_model(model, to_file='plot_model.png', show_shapes=True, show_layer_names=True)
+            
+            callbacks = [
+                ModelCheckpoint(filepath, monitor='val_loss', verbose=0, 
+                                save_best_only=True, save_weights_only=True, mode='min'),
+                ReduceLROnPlateau(monitor="val_loss", factor=0.90, patience=20, verbose=0, min_lr=0.0001),  
+                # EarlyStopping(monitor='val_loss', verbose=1, mode='min', patience=patience)
+            ]
+            history = model.fit(X_train, y_train_onehot, validation_data=(X_val, y_val_onehot), 
+                                epochs=epochs, batch_size=batch_size, callbacks=callbacks, verbose=0)
+           
+            # Evaluate the performance of the trained model based on the validation data
+            # Here we load the Trained weights from the file saved in the hard 
+            # disk, which should be the same as the weights of the current model.
+            model.load_weights(filepath)
+            y_pred = model.predict(X_val)
+
+            if from_logits:
+                y_pred = tf.nn.softmax(y_pred).numpy().argmax(axis=-1)
+            else:
+                y_pred = y_pred.argmax(axis=-1)
+                
+            labels = y_val_onehot.argmax(axis=-1)
+            acc[sub, train]  = accuracy_score(labels, y_pred)
+            kappa[sub, train] = cohen_kappa_score(labels, y_pred)
+                        
+            # Get the current 'OUT' time to calculate the 'run' training time
+            out_run = time.time()
+            # Print & write performance measures for each run
+            info = 'Subject: {}   seed {}   time: {:.1f} m   '.format(sub+1, train+1, ((out_run-in_run)/60))
+            info = info + 'valid_acc: {:.4f}   valid_loss: {:.3f}'.format(acc[sub, train], min(history.history['val_loss']))
+            print(info)
+            log_write.write(info +'\n')
+            # If current training run is better than previous runs, save the history.
+            if(BestSubjAcc < acc[sub, train]):
+                 BestSubjAcc = acc[sub, train]
+                 bestTrainingHistory = history
+        
+        # Store the path of the best model among several runs
+        best_run = np.argmax(acc[sub,:])
+        filepath = '/saved models/run-{}/subject-{}.h5'.format(best_run+1, sub+1)+'\n'
+        best_models.write(filepath)
+
+        # Plot Learning curves 
+        if (LearnCurves == True):
+            print('Plot Learning Curves ....... ')
+            draw_learning_curves(bestTrainingHistory, sub+1)
+          
+    # Get the current 'OUT' time to calculate the overall training time
+    out_exp = time.time()
+           
+    # Print & write the validation performance using all seeds
+    head1 = head2 = '         '
+    for sub in range(n_sub): 
+        head1 = head1 + 'sub_{}   '.format(sub+1)
+        head2 = head2 + '-----   '
+    head1 = head1 + '  average'
+    head2 = head2 + '  -------'
+    info = '\n---------------------------------\nValidation performance (acc %):'
+    info = info + '\n---------------------------------\n' + head1 +'\n'+ head2
+    for run in range(n_train): 
+        info = info + '\nSeed {}:  '.format(run+1)
+        for sub in range(n_sub): 
+            info = info + '{:.2f}   '.format(acc[sub, run]*100)
+        info = info + '  {:.2f}   '.format(np.average(acc[:, run])*100)
+    info = info + '\n---------------------------------\nAverage acc - all seeds: '
+    info = info + '{:.2f} %\n\nTrain Time  - all seeds: {:.1f}'.format(np.average(acc)*100, (out_exp-in_exp)/(60))
+    info = info + ' min\n---------------------------------\n'
+    print(info)
+    log_write.write(info+'\n')
+
+    # Close open files 
+    best_models.close()   
+    log_write.close() 
